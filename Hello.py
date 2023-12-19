6c1964fea761790f52b20dba61f5253f10418578
nurfatoni32768 <154314094+nurfatoni32768@users.noreply.github.com>
2023-12-19 20:12:40 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,28 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+def main():
+    st.title("kalkulator")
 
+    num1 = st.number_input("Angka Pertama")
+    num2 = st.number_input("Angka Kedua")
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+    operation = st.selectbox("Operasi:", ["Penjumlahan", "Pengurangan", "Perkalian", "Pembagian"])
 
-    st.write("# Welcome to Streamlit! 👋")
+    result = 0
 
-    st.sidebar.success("Select a demo above.")
+    if operation == "Penjumlahan":
+        result = num1 + num2
+    elif operation == "Pengurangan":
+        result = num1 - num2
+    elif operation == "Perkalian":
+        result = num1 * num2
+    elif operation == "Pembagian":
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            st.error("Pembagian dengan 0 tidak valid!")
+    
+    st.write("Hasil:", result)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+if __name__ == '__main__':
+    main()
